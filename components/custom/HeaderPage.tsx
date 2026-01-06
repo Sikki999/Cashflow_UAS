f78a45a11cb6445bd1acbb6328352f8a0d747445
Sikki999 <sikkifirdaus@gmail.com>
2026-01-06 10:30:45 +0700

    Update debt screen, UI table, and related components

--- components/custom/HeaderPage.tsx
+++ components/custom/HeaderPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from 'expo-router'
+import { RelativePathString, router, useNavigation } from 'expo-router'
 import { ChevronLeft, UserRoundPen } from 'lucide-react-native'
 import React from 'react'
 import { Pressable, Text, View } from 'react-native'
@@ -7,15 +7,16 @@ import { HStack } from '../ui/hstack'
 
 interface headerPropsType {
     title: string,
+    routeBack?: string
 }
-export default function HeaderPage({ title }: headerPropsType) {
+export default function HeaderPage({ title, routeBack }: headerPropsType) {
     const navigation = useNavigation();
     return (
         <>
             <View className='mt-[30px] px-[30px]'>
                 <Box>
                     <HStack className='items-center justify-between'>
-                        <Pressable onPress={()=>navigation.goBack()}>
+                         <Pressable onPress={()=> routeBack ? router.push(routeBack as RelativePathString) : navigation.goBack()}>
                             <HStack className='items-center'>
                                 <ChevronLeft color={'#4B4B4B'} />
                                 <Text className='text-lg text-[#4b4b4b]' style={{ fontFamily: "HankenGrotesk_300Light" }}>Back</Text>
